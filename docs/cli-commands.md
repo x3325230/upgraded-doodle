b4673cd4449f16065119ab9a3f53eefcce6bf22a
x3325230 <583001229@qq.com>
2022-04-11 04:27:55 +0800

    Initialize pipeline

---
+++ docs/cli-commands.md
@@ -0,0 +1,277 @@
+<!-- NOTE: Keep descriptions in sync with codes. -->
+
+# CLI commands
+
+- [install](#install-command)
+- [uninstall](#uninstall-command)
+- [start](#start-command)
+- [stop](#stop-command)
+- [restart](#restart-command)
+- [status](#status-command)
+- [test](#test-command)
+- [refresh](#refresh-command)
+- [customize](#customize-command)
+
+## `install` command
+
+Installs the service.
+
+### Usage
+
+```console
+winsw install [<path-to-config>] [--no-elevate] [--user|--username <username>] [--pass|--password <password>]
+```
+
+### Arguments
+
+`path-to-config`
+
+The path to the configuration file.
+If a file isn't specified, WinSW searches the executable directory for a *.xml* file with the same file name without the extension.
+
+### Options
+
+- `--no-elevate`
+
+  Doesn't automatically trigger a UAC prompt.
+
+- `--user|--username <username>`
+
+  Specifies the user name of the service account.
+
+- `--pass|--password <password>`
+
+  Specifies the password of the service account.
+
+## `uninstall` command
+
+Uninstalls the service.
+
+### Usage
+
+```console
+winsw uninstall [<path-to-config>] [--no-elevate]
+```
+
+### Arguments
+
+`path-to-config`
+
+The path to the configuration file.
+If a file isn't specified, WinSW searches the executable directory for a *.xml* file with the same file name without the extension.
+
+### Options
+
+- `--no-elevate`
+
+  Doesn't automatically trigger a UAC prompt.
+
+## `start` command
+
+Starts the service.
+
+### Usage
+
+```console
+winsw start [<path-to-config>] [--no-elevate]
+```
+
+### Arguments
+
+`path-to-config`
+
+The path to the configuration file.
+If a file isn't specified, WinSW searches the executable directory for a *.xml* file with the same file name without the extension.
+
+### Options
+
+- `--no-elevate`
+
+  Doesn't automatically trigger a UAC prompt.
+
+## `stop` command
+
+Stops the service.
+
+### Usage
+
+```console
+winsw stop [<path-to-config>] [--no-elevate] [--no-wait]
+```
+
+### Arguments
+
+`path-to-config`
+
+The path to the configuration file.
+If a file isn't specified, WinSW searches the executable directory for a *.xml* file with the same file name without the extension.
+
+### Options
+
+- `--no-elevate`
+
+  Doesn't automatically trigger a UAC prompt.
+
+- `--no-wait`
+
+  Doesn't wait for the service to actually stop.
+
+- `--force`
+
+  Stops the service even if it has started dependent services.
+
+## `restart` command
+
+Stops and then starts the service.
+
+### Usage
+
+```console
+winsw restart [<path-to-config>] [--no-elevate]
+```
+
+### Arguments
+
+`path-to-config`
+
+The path to the configuration file.
+If a file isn't specified, WinSW searches the executable directory for a *.xml* file with the same file name without the extension.
+
+### Options
+
+- `--no-elevate`
+
+  Doesn't automatically trigger a UAC prompt.
+
+- `--force`
+
+  Restarts the service even if it has started dependent services.
+
+## `status` command
+
+Checks the status of the service.
+
+### Usage
+
+```console
+winsw status [<path-to-config>]
+```
+
+### Arguments
+
+`path-to-config`
+
+The path to the configuration file.
+If a file isn't specified, WinSW searches the executable directory for a *.xml* file with the same file name without the extension.
+
+## `test` command
+
+Checks if the service can be started and then stopped without installation.
+
+### Usage
+
+```console
+winsw test [<path-to-config>] [--no-elevate] [--timeout <timeout>] [--no-break]
+```
+
+### Arguments
+
+`path-to-config`
+
+The path to the configuration file.
+If a file isn't specified, WinSW searches the executable directory for a *.xml* file with the same file name without the extension.
+
+### Options
+
+- `--no-elevate`
+
+  Doesn't automatically trigger a UAC prompt.
+
+- `--timeout <timeout>`
+
+  Specifies the number of seconds to wait before the service is stopped.
+  If not specified or -1 is specified, WinSW waits for a keystroke indefinitely.
+
+- `--no-break`
+
+  Ignores keystrokes.
+  If specified, WinSW waits for Ctrl+C.
+
+## `refresh` command
+
+Refreshes the service properties without reinstallation.
+
+### Usage
+
+```console
+winsw refresh [<path-to-config>] [--no-elevate]
+```
+
+### Arguments
+
+`path-to-config`
+
+The path to the configuration file.
+If a file isn't specified, WinSW searches the executable directory for a *.xml* file with the same file name without the extension.
+
+### Options
+
+- `--no-elevate`
+
+  Doesn't automatically trigger a UAC prompt.
+
+## `customize` command
+
+Customizes the wrapper executable.
+
+### Usage
+
+```console
+winsw customize -o|--output <output> --manufacturer <manufacturer>
+```
+
+### Options
+
+- `-o|--output <output>`
+
+  Required. Specifies the path to the output file.
+
+- `--manufacturer <manufacturer>`
+
+  Specifies the manufacturer name of the customized executable.
+
+## `dev ps` command
+
+Draws the process tree associated with the service.
+
+### Usage
+
+```console
+winsw dev ps [<path-to-config>] [-a|--all]
+```
+
+### Options
+
+- `-a|--all`
+
+  Optional. Draws the process tree associated with all services.
+
+## `dev kill` command
+
+Terminates the service if it has stopped responding.
+
+### Usage
+
+```console
+winsw dev kill [<path-to-config>] [--no-elevate]
+```
+
+## `dev list` command
+
+Lists services managed by the current executable.
+
+### Usage
+
+```console
+winsw dev list
+```
---
+++ src/WinSW.Core/Util/ProcessExtensions.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using log4net;
+using WinSW.Native;
+using static WinSW.Native.ConsoleApis;
+using static WinSW.Native.ProcessApis;
+
+namespace WinSW.Util
+{
+    public static class ProcessExtensions
+    {
+        private static readonly ILog Log = LogManager.GetLogger(typeof(ProcessExtensions));
+
+        public static void StopTree(this Process process, int millisecondsTimeout)
+        {
+            StopPrivate(process, millisecondsTimeout);
+
+            foreach (var child in GetChildren(process))
+            {
+                using (child)
+                {
+                    StopTree(child, millisecondsTimeout);
+                }
+            }
+        }
+
+        internal static void StopDescendants(this Process process, int millisecondsTimeout)
+        {
+            foreach (var child in GetChildren(process))
+            {
+                using (child)
+                {
+                    StopTree(child, millisecondsTimeout);
+                }
+            }
+        }
+
+        internal static unsafe List<Process> GetChildren(this Process process)
+        {
+            var startTime = process.StartTime;
+            int processId = process.Id;
+
+            var children = new List<Process>();
+
+            foreach (var other in Process.GetProcesses())
+            {
+                try
+                {
+                    if (other.StartTime <= startTime)
+                    {
+                        goto Next;
+                    }
+
+                    var handle = other.Handle;
+
+                    if (NtQueryInformationProcess(
+                        handle,
+                        PROCESSINFOCLASS.ProcessBasicInformation,
+                        out var information,
+                        sizeof(PROCESS_BASIC_INFORMATION)) != 0)
+                    {
+                        goto Next;
+                    }
+
+                    if ((int)information.InheritedFromUniqueProcessId == processId)
+                    {
+                        Log.Debug($"Found child process '{other.Format()}'.");
+                        children.Add(other);
+                        continue;
+                    }
+
+                Next:
+                    other.Dispose();
+                }
+                catch (Exception e) when (e is InvalidOperationException || e is Win32Exception)
+                {
+                    other.Dispose();
+                }
+            }
+
+            return children;
+        }
+
+        // true  => canceled
+        // false => terminated
+        // null  => finished
+        internal static bool? Stop(this Process process, int millisecondsTimeout)
+        {
+            if (process.HasExited)
+            {
+                return null;
+            }
+
+            if (!(SendCtrlC(process) is bool sent))
+            {
+                return null;
+            }
+
+            if (!sent)
+            {
+                try
+                {
+                    sent = process.CloseMainWindow();
+                }
+                catch (InvalidOperationException)
+                {
+                    return null;
+                }
+            }
+
+            if (sent)
+            {
+                if (process.WaitForExit(millisecondsTimeout))
+                {
+                    return true;
+                }
+            }
+
+#if NET
+            process.Kill();
+#else
+            try
+            {
+                process.Kill();
+            }
+            catch when (process.HasExited)
+            {
+            }
+#endif
+
+            return false;
+        }
+
+        private static void StopPrivate(Process process, int millisecondsTimeout)
+        {
+            Log.Debug($"Stopping process '{process.Format()}'...");
+
+            if (process.HasExited)
+            {
+                goto Exited;
+            }
+
+            if (!(SendCtrlC(process) is bool sent))
+            {
+                goto Exited;
+            }
+
+            if (!sent)
+            {
+                try
+                {
+                    sent = process.CloseMainWindow();
+                }
+                catch (InvalidOperationException)
+                {
+                    goto Exited;
+                }
+            }
+
+            if (sent)
+            {
+                if (process.WaitForExit(millisecondsTimeout))
+                {
+                    Log.Debug($"Process '{process.Format()}' canceled with code {process.ExitCode}.");
+                    return;
+                }
+            }
+
+#if NET
+            process.Kill();
+#else
+            try
+            {
+                process.Kill();
+            }
+            catch when (process.HasExited)
+            {
+            }
+#endif
+
+            Log.Debug($"Process '{process.Format()}' terminated.");
+            return;
+
+        Exited:
+            Log.Debug($"Process '{process.Format()}' has already exited.");
+        }
+
+        private static bool? SendCtrlC(Process process)
+        {
+            if (!AttachConsole(process.Id))
+            {
+                int error = Marshal.GetLastWin32Error();
+                switch (error)
+                {
+                    // The process does not have a console.
+                    case Errors.ERROR_INVALID_HANDLE:
+                        return false;
+
+                    // The process has exited.
+                    case Errors.ERROR_INVALID_PARAMETER:
+                        return null;
+
+                    // The calling process is already attached to a console.
+                    case Errors.ERROR_ACCESS_DENIED:
+                    default:
+                        Log.Warn("Failed to attach to console. " + new Win32Exception(error).Message);
+                        return false;
+                }
+            }
+
+            // Don't call GenerateConsoleCtrlEvent immediately after SetConsoleCtrlHandler.
+            // A delay was observed as of Windows 10, version 2004 and Windows Server 2019.
+            _ = GenerateConsoleCtrlEvent(CtrlEvents.CTRL_C_EVENT, 0);
+
+            bool succeeded = FreeConsole();
+            Debug.Assert(succeeded);
+
+            return true;
+        }
+    }
+}
